51566e9984c5470995abce99f2832fc8e5f7616d
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-02-23 23:54:55 +0200

    feat: add complaints to database for admin to see

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,30 +1,47 @@
+// import { useState, useEffect } from 'react';
+import supabase from '../../helper/supabaseClient';
 import './Contact.css';
 import { useFormik } from 'formik';
 import { toast } from 'sonner';
+import '../../helper/i18n';
+import { useTranslation } from 'react-i18next';
 
 export default function Contact() {
+  const { t } = useTranslation();
+
+  const addToDatabase = async (values) => {
+    const { data, error } = await supabase
+      .from('responses-to-form')
+      .insert([{ text: values.text, email: values.email }]);
+
+    if (error) throw error;
+    return data;
+  };
+
   const formik = useFormik({
     initialValues: {
       email: '',
       text: '',
     },
-    onSubmit: () => {
-      toast.success('Mesaj trimis cu succes!', { position: 'bottom-right' });
+    onSubmit: (value) => {
+      addToDatabase(value);
+      toast.success(t('toast-success'), { position: 'bottom-right' });
       formik.resetForm();
     },
   });
 
   return (
     <div className="contact-container">
-      <h1 className="contact-title">Contactează-ne</h1>
+      <h1 className="contact-title">{t('contact-us')}</h1>
       <form className="contact-form" onSubmit={formik.handleSubmit}>
         <div className="input-group">
           <label htmlFor="email">Email</label>
           <input
+            required
             id="email"
             name="email"
             type="email"
-            placeholder="EMAIL-UL TAU..."
+            placeholder={t('your-email')}
             onChange={formik.handleChange}
             value={formik.values.email}
             className="contact-input"
@@ -32,11 +49,12 @@ export default function Contact() {
         </div>
 
         <div className="input-group">
-          <label htmlFor="text">Mesaj</label>
+          <label htmlFor="text">{t('message')}</label>
           <textarea
+            required
             id="text"
             name="text"
-            placeholder="SCRIE MESAJUL DORIT..."
+            placeholder={t('your-message')}
             onChange={formik.handleChange}
             value={formik.values.text}
             className="contact-input contact-textarea"
@@ -44,7 +62,7 @@ export default function Contact() {
         </div>
 
         <button className="custom-btn" type="submit">
-          Trimite
+          {t('submit-btn')}
         </button>
       </form>
     </div>
